5b780f44e9db96aa933f416babfdba04933f6e67
Mgordino <gordino.miguel@gmail.com>
2017-09-12 16:48:00 +0200

    16:47

--- ProxiBanque/src/main/java/com/gor_lak/filRouge/ProxiBanque/presentation/fenetre/App.java
+++ ProxiBanque/src/main/java/com/gor_lak/filRouge/ProxiBanque/presentation/fenetre/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldfdf  bldqzdqdda!" );
+        System.out.println( "Hello Worldfdf  bldqzdqdDZQDda!" );
     }
 }
