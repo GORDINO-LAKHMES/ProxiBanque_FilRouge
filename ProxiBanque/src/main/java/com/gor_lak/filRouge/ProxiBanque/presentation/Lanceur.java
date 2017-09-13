eb28ffa70c9c33f0f7af4de0ae45063d52a12559
Mgordino <gordino.miguel@gmail.com>
2017-09-13 10:36:25 +0200

    10:36 "FenetreConnexion"

--- ProxiBanque/src/main/java/com/gor_lak/filRouge/ProxiBanque/presentation/Lanceur.java
+++ ProxiBanque/src/main/java/com/gor_lak/filRouge/ProxiBanque/presentation/Lanceur.java
@@ -3,6 +3,7 @@ package com.gor_lak.filRouge.ProxiBanque.presentation;
 import java.util.Scanner;
 
 import com.gor_lak.filRouge.ProxiBanque.metier.visa;
+import com.gor_lak.filRouge.ProxiBanque.presentation.fenetre.FenetreConnexion;
 import com.gor_lak.filRouge.ProxiBanque.service.IserviceAuditeur;
 import com.gor_lak.filRouge.ProxiBanque.service.IserviceConseiller;
 import com.gor_lak.filRouge.ProxiBanque.service.IserviceGerant;
@@ -20,51 +21,10 @@ import com.gor_lak.filRouge.ProxiBanque.service.ServiceGerant;
 public class Lanceur {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		Scanner clavier= new Scanner(System.in);
-		//Object visaP=visa.visaP; Object visaE=visa.visaE;
-		Object Carte[]= {visa.visaP, visa.visaE};
+		FenetreConnexion f = new FenetreConnexion();
 		
-		System.out.println("Veillez saisir 0 pour choisir la VisaE et 1 pour la VisaP");
-		int n= clavier.nextInt();
-		System.out.println("\nVous avez choisi la cate "+ "[ "+Carte[n]+" ]");
-		
-		System.out.println("\n\n=================================================Auditeur======================================================1");
-		
-		IserviceAuditeur isa= new ServiceAuditeur();
-		isa.lireCompteAuditeur (null);
-		
-		System.out.println("=================================================Gerant=========================================================3\n");
-		IserviceGerant isg=new ServiceGerant();
-		
-		isg.seConnecter(null, null);
-		isg.ajouterClient(null);
-		isg.attribuerClient(null);
-		isg.creerCompte(null);
-		isg.lireCompte(null);
-		isg.modifierCompte(null);
-		isg.simulationCompte(null);
-		isg.patrimoineCompte(null);
-		isg.virserment(null,null);
-		isg.supprimerCompte(null);
-		isg.ajouterConseiller(null);
-		isg.modifierConseiller(null);
-		isg.supprimerConseiller(null);
-		System.out.println("=================================================Conseiller====================================================2\n");
-		IserviceConseiller isc= new ServiceConsseiller();
-		
-		isc.seConnecter(null, null);
-		isc.ajouterClient(null);
-		isc.attribuerClient(null);
-		isc.creerCompte(null);
-		isc.lireCompte(null);
-		isc.modifierCompte(null);
-		isc.simulationCompte(null);
-		isc.patrimoineCompte(null);
-		isc.virserment(null, null);
-		isc.supprimerCompte(null);
-		
+		f.setVisible(true);
 	}
 
 }
