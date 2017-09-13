f3cd2fc28541eaf314a72dfe39b7edd5c8c366e3
Mgordino <gordino.miguel@gmail.com>
2017-09-13 10:02:43 +0200

    10:02

---
+++ ProxiBanque/src/main/java/com/gor_lak/filRouge/ProxiBanque/presentation/fenetre/Fenetre.java
@@ -0,0 +1,10 @@
+package com.gor_lak.filRouge.ProxiBanque.presentation.fenetre;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Fenetre extends JFrame {
+	
+	private int i = 0;
+	// fdsfdzq
+}
